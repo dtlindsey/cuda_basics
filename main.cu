c4315f618ee7d7c076d9fc8494b69ccc1a7a3180
Daniel Lindsey <dtlindsey@gmail.com>
2020-08-18 21:20:37 -0700

    add function

--- main.cu
+++ main.cu
@@ -1,11 +1,29 @@
 // #include <iostream>
 #include <stdio.h>
 
-__global__ void kernel( void ) {
+#include "./common/book.h"
+
+__global__ void add( int a, int b, int *c ) {
+    *c = a + b;
 }
 
-int main() {
-    kernel<<<1,1>>>();
-    printf("Hello World;\n");
+int main( void ) {
+    int c;
+    int *dev_c;
+
+    HANDLE_ERROR( cudaMalloc((void**) &dev_c, sizeof(int)));
+
+    add<<<1,1>>>(2, 7, dev_c);
+
+    HANDLE_ERROR( cudaMemcpy(
+            &c,
+            dev_c,
+            sizeof(int),
+            cudaMemcpyDeviceToHost
+            ));
+
+    printf("2+7= %d\n", c);
+    cudaFree(dev_c);
+
     return 0;
 }
