8d9eafc52a316a665d305023e2237b3861331c9a
Daniel Lindsey <dtlindsey@gmail.com>
2020-08-23 21:59:50 -0700

    cannot seem to get the c var to print properly.  need to debug in cuda

---
+++ ch4.cu
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "./common/book.h"
+
+#define NUM_THREADS 10
+
+// cuda
+__global__ void add(int *a, int *b, int *c) {
+    int t_id = blockIdx.x;
+    if (t_id < NUM_THREADS) {
+        printf("GPU %d * %d = %d",a[t_id], b[t_id], c[t_id]);
+        c[t_id] = a[t_id] * b[t_id];
+    }
+}
+
+int main( void ) {
+    int a[NUM_THREADS], b[NUM_THREADS], c[NUM_THREADS];
+    int *dev_a, *dev_b, *dev_c;
+
+    // allocate the memory on the GPU
+    HANDLE_ERROR( cudaMalloc( &dev_a, NUM_THREADS * sizeof(int) ) );
+    HANDLE_ERROR( cudaMalloc( &dev_b, NUM_THREADS * sizeof(int) ) );
+    HANDLE_ERROR( cudaMalloc( &dev_c, NUM_THREADS * sizeof(int) ) );
+
+    // fill the arrays 'a' and 'b' on the CPU
+    for (int i=0; i<NUM_THREADS; i++) {
+        a[i] = -i;
+        b[i] = i * i;
+        c[i] = -100;
+    }
+
+    // copy the arrays 'a' and 'b' to the GPU
+    HANDLE_ERROR( cudaMemcpy( dev_a, a, NUM_THREADS * sizeof(int),
+                              cudaMemcpyHostToDevice ) );
+    HANDLE_ERROR( cudaMemcpy( dev_b, b, NUM_THREADS * sizeof(int),
+                              cudaMemcpyHostToDevice ) );
+
+    add<<<NUM_THREADS,1>>>( dev_a, dev_b, dev_c );
+
+    // copy the array 'c' back from the GPU to the CPU
+    HANDLE_ERROR( cudaMemcpy( c, dev_c, NUM_THREADS * sizeof(int),
+                              cudaMemcpyDeviceToHost ) );
+
+    // display the results
+    for (int i=0; i<NUM_THREADS; i++) {
+        printf( "%d + %d = %d\n", a[i], b[i], c[i] );
+    }
+
+    // free the memory allocated on the GPU
+    HANDLE_ERROR( cudaFree( dev_a ) );
+    HANDLE_ERROR( cudaFree( dev_b ) );
+    HANDLE_ERROR( cudaFree( dev_c ) );
+
+    return 0;
+}
+//
+//int main(void) {
+//    int a[NUM_THREADS], b[NUM_THREADS], c[NUM_THREADS];
+//    int *dev_a, *dev_b, *dev_c;
+//
+////    time_t start, end;
+//    // add data on cpu
+////    start = time(0x0);
+//
+//    HANDLE_ERROR(cudaMalloc((void** ) &dev_a,
+//                            (NUM_THREADS * sizeof(int))));
+//    HANDLE_ERROR(cudaMalloc((void** ) &dev_b,
+//                            (NUM_THREADS * sizeof(int))));
+//    HANDLE_ERROR(cudaMalloc((void** ) &dev_c,
+//                            (NUM_THREADS * sizeof(int))));
+//
+//    // fill the array
+//    for (int i = 0; i<NUM_THREADS; i++) {
+//        a[i] = -1;
+//        b[i] = i * i;
+//    }
+//
+//    HANDLE_ERROR(cudaMemcpy(dev_a,
+//                            a,
+//                            (NUM_THREADS * sizeof(int)),
+//                            cudaMemcpyHostToDevice));
+//    HANDLE_ERROR(cudaMemcpy(dev_b,
+//                            b,
+//                            (NUM_THREADS * sizeof(int)),
+//                            cudaMemcpyHostToDevice));
+//
+//    add<<<NUM_THREADS, 1>>>(dev_a, dev_b, dev_c);
+////    printf("testing %d", dev_c[0]);
+//    HANDLE_ERROR(cudaMemcpy(c,
+//                            dev_c,
+//                            (NUM_THREADS * sizeof(int)),
+//                            cudaMemcpyDeviceToHost));
+//
+////    end = time(0x0) - start;
+//
+//    for (int i = 0; i < NUM_THREADS; i++) {
+//        printf("%d * %d = %d\n",a[i], b[i], c[i]);
+//    }
+////    printf("Took %ld seconds to compute\n",end);
+//
+//    HANDLE_ERROR(cudaFree(dev_a));
+//    HANDLE_ERROR(cudaFree(dev_b));
+//    HANDLE_ERROR(cudaFree(dev_c));
+//
+//    return 0;
+//}
--- main.cu
+++ main.cu
@@ -8,54 +8,22 @@ __global__ void add( int a, int b, int *c ) {
 }
 
 int main( void ) {
-    int count;
     cudaDeviceProp prop;
+    int device;
 
-    HANDLE_ERROR(cudaGetDeviceCount(&count));
-    printf("Device count: %d\n", count);
+    HANDLE_ERROR(cudaGetDevice(&device));
+    printf("ID of current device: %d\n", device);
 
-    if (count){
-        for (int i =0; i< count; i++) {
-            HANDLE_ERROR(
-                    cudaGetDeviceProperties(&prop, i)
-            );
-            printf("---------General Device Info for device %d--------------\n", i);
-            printf("Name:    %s\n", prop.name);
-            printf("Compute capability: %d.%d\n", prop.major, prop.minor);
-            printf("Clock rate: %d\n", prop.clockRate);
-            printf("Device copy overlap: ");
-            if (prop.deviceOverlap) {
-                printf("Enabled\n");
-            } else {
-                printf("Disabled\n");
-            }
-            printf("Kernel execution timeout: ");
-            if (prop.kernelExecTimeoutEnabled) {
-                printf("Enabled\n");
-            } else {
-                printf("Disabled\n");
-            }
-            printf("--- Memory Information for device %d ---\n", i);
-            printf("Total global mem: %1d\n", prop.totalGlobalMem);
-            printf("Total constant mem: %1d\n", prop.totalConstMem);
-            printf("Max mem pitch: %1d\n", prop.memPitch);
-            printf("Texture Alignment: %1d\n", prop.textureAlignment);
-            printf(" --- MP Information for device %d ---\n", i);
-            printf("Multiprocessor count: %d\n",
-                   prop.multiProcessorCount);
-            printf("Shared mem per mp: %1d\n", prop.sharedMemPerBlock);
-            printf("Registers per mp: %d\n", prop.regsPerBlock);
-            printf("Threads in warp: %d\n", prop.warpSize);
-            printf("Max threads per block: %d\n",
-                   prop.maxThreadsPerBlock);
-            printf("Max thread dimensions: (%d, %d, %d)\n",
-                   prop.maxThreadsDim[0], prop.maxThreadsDim[1],
-                   prop.maxThreadsDim[2]);
-            printf("Max grid dimensions: (%d, %d, %d)\n",
-                   prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
-            printf("\n");
-        }
-    }
+    memset(&prop, 0, sizeof(cudaDeviceProp));
+
+    prop.major = 7;
+    prop.minor = 1;
+
+    HANDLE_ERROR(cudaChooseDevice(&device, &prop));
+
+    printf("ID of device with revisions of data 7.1 or above: %d\n", device);
+
+    HANDLE_ERROR(cudaSetDevice(device));
 
     return 0;
 }
