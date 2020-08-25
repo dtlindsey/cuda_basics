1f5372d2daca2b083dcb2a810b60d696b901b2b2
Daniel Lindsey <dtlindsey@gmail.com>
2020-08-25 03:28:25 -0700

    got image to display on cpu

--- ch4.2.cu
+++ ch4.2.cu
@@ -4,7 +4,7 @@
 #include "./common/book.h"
 #include "./common/cpu_bitmap.h"
 
-#define DIM 1000
+#define DIM 2048
 
 struct cuComplex {
     float r;
@@ -12,7 +12,7 @@ struct cuComplex {
     cuComplex(float a, float b) : r(a), i(b) {}
     float magnitude2 (void) { return r * r + i * i; }
     cuComplex operator* (const cuComplex& a) {
-        return cuComplex(r * a.r - i * a.i, i * a.r + r * a.i);
+        return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
     }
     cuComplex operator+ (const cuComplex& a) {
         return cuComplex(r + a.r, i + a.i);
@@ -21,16 +21,16 @@ struct cuComplex {
 
 int julia( int x, int y) {
     const float scale = 1.5;
-    float jx = scale * (float)(DIM/2 - x )/ (DIM/2);
-    float jy = scale * (float)(DIM/2 - y )/ (DIM/2);
-
-    cuComplex c(0.8, 0.156);
+    float jx = scale * (((float)DIM/2 - x )/ (DIM/2));
+    float jy = scale * (((float)DIM/2 - y )/ (DIM/2));
+//    printf("jx:%f, jy:%f\n",jx, jy);
+    cuComplex c(-0.8, 0.156);
     cuComplex a(jx, jy);
 
     int i = 0;
     for (i = 0; i < 200; i++){
         a = a * a + c;
-        if (a.magnitude2() > 1000) {
+        if (a.magnitude2() > DIM) {
             return 0;
         }
     }
@@ -41,8 +41,8 @@ void kernel( unsigned char *ptr) {
     for (int y=0; y < DIM; y++) {
         for (int x=0; x < DIM; x++) {
             int offset = x + y * DIM;
-
             int juliaValue = julia(x, y);
+//            printf("juliaVal:%d\n",juliaValue);
             ptr[offset * 4 + 0] = 255 * juliaValue;
             ptr[offset * 4 + 1] = 0;
             ptr[offset * 4 + 2] = 0;
