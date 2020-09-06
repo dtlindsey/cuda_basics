27aa5c99b6fa58b04d9944df1e7e187b6551de52
Daniel Lindsey <dtlindsey@gmail.com>
2020-09-05 22:53:33 -0700

    gpu testing, memcpy not working devicetohost

--- ch4.2.cu
+++ ch4.2.cu
@@ -9,26 +9,44 @@
 struct cuComplex {
     float r;
     float i;
-    cuComplex(float a, float b) : r(a), i(b) {}
-    float magnitude2 (void) { return r * r + i * i; }
-    cuComplex operator* (const cuComplex& a) {
+    __device__ cuComplex(float a, float b) : r(a), i(b) {}
+    __device__ float magnitude2 (void) {
+        return r * r + i * i;
+    }
+    __device__ cuComplex operator* (const cuComplex& a) {
         return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
     }
-    cuComplex operator+ (const cuComplex& a) {
+    __device__ cuComplex operator+ (const cuComplex& a) {
         return cuComplex(r + a.r, i + a.i);
     }
 };
 
-int julia( int x, int y) {
+//int julia( int x, int y) {
+//    const float scale = 1.5;
+//    float jx = scale * (((float)DIM/2 - x )/ (DIM/2));
+//    float jy = scale * (((float)DIM/2 - y )/ (DIM/2));
+////    printf("jx:%f, jy:%f\n",jx, jy);
+//    cuComplex c(-0.8, 0.156); //c(-0.8, 0.156);
+//    cuComplex a(jx, jy);
+//
+//    int i = 0;
+//    for (i = 0; i < 350; i++){
+//        a = a * a + c;
+//        if (a.magnitude2() > DIM) {
+//            return 0;
+//        }
+//    }
+//    return 1;
+//}
+
+__device__ int julia(int x, int y) {
     const float scale = 1.5;
-    float jx = scale * (((float)DIM/2 - x )/ (DIM/2));
-    float jy = scale * (((float)DIM/2 - y )/ (DIM/2));
-//    printf("jx:%f, jy:%f\n",jx, jy);
-    cuComplex c(-0.8, 0.156); //c(-0.8, 0.156);
+    float jx = scale * (((float)DIM/2 - x) / (DIM/2));
+    float jy = scale * (((float)DIM/2 - y) / (DIM/2));
+    cuComplex c(-0.8, 0.156);
     cuComplex a(jx, jy);
 
-    int i = 0;
-    for (i = 0; i < 350; i++){
+    for (int i=0; i < 350; i++) {
         a = a * a + c;
         if (a.magnitude2() > DIM) {
             return 0;
@@ -37,26 +55,72 @@ int julia( int x, int y) {
     return 1;
 }
 
-void kernel( unsigned char *ptr) {
-    for (int y=0; y < DIM; y++) {
-        for (int x=0; x < DIM; x++) {
-            int offset = x + y * DIM;
-            int juliaValue = julia(y, x);
-//            printf("juliaVal:%d\n",juliaValue);
-            ptr[offset * 4 + 0] = 255 * juliaValue;
-            ptr[offset * 4 + 1] = 0;
-            ptr[offset * 4 + 2] = 255 * juliaValue;
-            ptr[offset * 4 + 3] = 255;
-        }
-    }
+__global__ void kernel (unsigned char *ptr) {
+    // map block.idx
+    int x = blockIdx.x;
+    int y = blockIdx.y;
+    int offset = x + y * gridDim.x;
+
+    //
+    int juliaValue = julia(y, x);
+    ptr[offset * 4 + 0] = 255 * juliaValue;
+    ptr[offset * 4 + 1] = 0;
+    ptr[offset * 4 + 2] = 0;
+    ptr[offset * 4 + 3] = 255;
 }
 
-int main(void) {
-    CPUBitmap bitmap (DIM, DIM);
-    unsigned char *ptr = bitmap.get_ptr();
+//void kernel( unsigned char *ptr) {
+//    for (int y=0; y < DIM; y++) {
+//        for (int x=0; x < DIM; x++) {
+//            int offset = x + y * DIM;
+//            int juliaValue = julia(y, x);
+////            printf("juliaVal:%d\n",juliaValue);
+//            ptr[offset * 4 + 0] = 255 * juliaValue;
+//            ptr[offset * 4 + 1] = 0;
+//            ptr[offset * 4 + 2] = 255 * juliaValue;
+//            ptr[offset * 4 + 3] = 255;
+//        }
+//    }
+//}
 
-    kernel(ptr);
+struct DataBlock {
+    unsigned  char *dev_bitmap;
+};
+
+int main(void) {
+    DataBlock data;
+    CPUBitmap bitmap(DIM, DIM, &data);
+//    unsigned char *ptr = bitmap.get_ptr();
+    unsigned char *dev_bitmap;
+
+    cudaError_t did_init = cudaMalloc( (void**) &dev_bitmap, (DIM * DIM * sizeof(char)));
+    if(did_init != cudaSuccess) {
+        printf("Data didn't get initialized\n");
+        return 1;
+    }
+
+    data.dev_bitmap = dev_bitmap;
+
+    dim3 grid(DIM, DIM);
+    kernel<<<grid, 1>>>(dev_bitmap);
+
+    cudaError_t did_copy = cudaMemcpy( bitmap.get_ptr(), dev_bitmap,
+                                bitmap.image_size(),
+                                cudaMemcpyDeviceToHost);
+
+    if (did_copy != cudaSuccess) {
+        printf("Didn't copy to host\n");
+        return 1;
+    }
 
     bitmap.display_and_exit();
+
+    bool did_free = cudaFree(dev_bitmap);
+    if(!did_free) {
+        printf("didn't free gpu memory\n");
+        return 1;
+    }
+
+    return 0;
 }
 
