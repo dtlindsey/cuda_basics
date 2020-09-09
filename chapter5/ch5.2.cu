02c33513de601d3df832a180045288dd91ba7d17
Daniel Lindsey <dtlindsey@gmail.com>
2020-09-08 22:39:50 -0700

    ventors longer than thread count on gpu

--- chapter5/ch5.2.cu
+++ chapter5/ch5.2.cu
@@ -2,12 +2,13 @@
 
 #include "../common/book.h"
 
-#define N 10000
+#define N (30 * 1024)
 
 __global__ void add(int *a, int *b, int *c) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid < N) {
+    while (tid < N) {
         c[tid] = a[tid] + b[tid];
+        tid += blockDim.x * gridDim.x;
     }
 }
 
@@ -20,9 +21,9 @@ int main(void) {
     HandleError(cudaMalloc(&dev_b, N * sizeof(int)), "cudaMalloc dev_b", 21);
     HandleError(cudaMalloc(&dev_c, N * sizeof(int)), "cudaMalloc dev_c", 22);
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
         a[i] = i;
-        b[i] = i * i * i;
+        b[i] = i * i;
     }
 
     // copy data
@@ -37,7 +38,7 @@ int main(void) {
                            cudaMemcpyHostToDevice),
                 "cudaMemcpy dev_b", 34);
 
-    add<<<(N + 127)/128,128>>>(dev_a, dev_b, dev_c);
+    add<<<128,128>>>(dev_a, dev_b, dev_c);
 
     // copy result back to host
     HandleError(cudaMemcpy(c,
