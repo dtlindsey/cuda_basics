5eade451dfc530db1488bade6764dbd0d50f22b6
Daniel Lindsey <dtlindsey@gmail.com>
2020-09-28 23:37:26 -0700

    chapter 5.3

--- chapter5/ch5.3.cu
+++ chapter5/ch5.3.cu
@@ -1,72 +1,88 @@
 #include <stdio.h>
 
-#include "cuda.h"
 #include "../common/book.h"
 
-#define DIM 1024
-
-#define imin(a, b) (a<b?a:b)
+#define imin(a, b) (a<b ? a:b)
+#define sum_squares(x) (x*(x+1) * (2 * x +1)/6)
 
 const int N = 33 * 1024;
 const int threadsPerBlock = 256;
-const int blocksPerGrid =
-        imin( 32,(N + threadsPerBlock -1) / threadsPerBlock);
+const int blocksPerGrid = imin(32, (N + threadsPerBlock-1) / threadsPerBlock);
 
 __global__ void dot(float *a, float *b, float *c) {
     __shared__ float cache[threadsPerBlock];
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     int cacheIndex = threadIdx.x;
+
     float temp = 0;
     while (tid < N) {
-        temp += a[tid] + b[tid];
+        temp += a[tid] * b[tid];
         tid += blockDim.x * gridDim.x;
     }
-
-    // set cache value
+    // set cache values
     cache[cacheIndex] = temp;
 
-    // sync
-    __syncthreads();
+    // sync threads
+    __syncthreads_and(0);
 
+    // for reduction threadsPerBlock must be power of two
     int i = blockDim.x / 2;
     while (i != 0) {
         if (cacheIndex < i) {
-            cache[cacheIndex] += cacheIndex[cacheIndex + i];
+            cache[cacheIndex] += cache[cacheIndex + i];
         }
-        __syncthreads();
+        __syncthreads_and(0);
         i /= 2;
     }
+    if (cacheIndex == 0) {
+        c[blockIdx.x] = cache[0];
+    }
 }
 
-int main( void ) {
-    float *a, *b, *partial_c;
+int main(void) {
+    float *a, *b, c, *partial_c;
     float *dev_a, *dev_b, *dev_partial_c;
 
-    a = (float*)malloc(N * sizeof(float));
-    b = (float*)malloc( N * sizeof(float));
-    partial_c = (float*) malloc( blocksPerGrid * sizeof(float));
+    // allocate memory on the cpi
+    a = (float*)malloc(N*sizeof(float));
+    b = (float*)malloc(N*sizeof(float));
+    partial_c = (float*)malloc(N*sizeof(float));
 
-    // allocate mem
-    HANDLE_ERROR(cudaMalloc((void**) &dev_a,
-                            N * sizeof(float)));
-    HANDLE_ERROR(cudaMalloc((void**) &dev_b,
-                            N * sizeof(float)));
-    HANDLE_ERROR(cudaMalloc((void**) &dev_partial_c,
-                            blocksPerGrid * sizeof(float)));
+    //allocate the memory on the GPU
+    HANDLE_ERROR(cudaMalloc((void**) &dev_a, N*sizeof(float)));
+    HANDLE_ERROR(cudaMalloc((void**) &dev_b, N*sizeof(float)));
+    HANDLE_ERROR(cudaMalloc((void**) &dev_partial_c, blocksPerGrid*sizeof(float)));
 
-    // fill host
+    // fill in the host memory
     for (int i = 0; i < N; i++) {
         a[i] = i;
-        b[i] = i * i;
+        b[i] = i * 2;
     }
 
-    HANDLE_ERROR(cudaMemcpy(dev_a, a, N * sizeof(float), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(dev_b, b, N * sizeof(float), cudaMemcpyHostToDevice));
+    // copy the array to 'a' and 'b' to the GPU
+    HANDLE_ERROR(cudaMemcpy(dev_a, a, N*sizeof(float), cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMemcpy(dev_b, b, N*sizeof(float), cudaMemcpyHostToDevice));
+    dot<<<blocksPerGrid, threadsPerBlock>>>(dev_a, dev_b, dev_partial_c);
 
-    dot<<<blocksPerGrid, threadsPerBlock>>> (dev_a,
-                                             dev_b,
-                                             dev_partial_c);
+    // copy the array data
+    HANDLE_ERROR(cudaMemcpy(partial_c, dev_partial_c, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost));
 
+    // finish up on the CPU
+    c = 0;
+    for (int i=0; i < blocksPerGrid; i++) {
+        c += partial_c[i];
+    }
+
+    printf("Done GPU value %.6g = %.6g?\n", c,
+           2 * sum_squares((float)N - 1));
+
+    // free memory on GPU
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_partial_c);
+
+    // free memory on the CPU
+    free(a);
+    free(b);
+    free(partial_c);
 }
-
-
